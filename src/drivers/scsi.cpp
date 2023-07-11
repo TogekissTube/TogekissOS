1531e15656735bf7f360aca6a5baeb32d8908d16
TogekissTube <138976298+TogekissTube@users.noreply.github.com>
2023-07-11 21:25:20 +0200

    New funcions

--- src/drivers/scsi.cpp
+++ src/drivers/scsi.cpp
@@ -7,7 +7,8 @@
 
 #define SCSI_DEVICE_PATH "/dev/sg0"  // SCSI device path
 
-bool readDataSCSI(int sector, int length, std::vector<uint8_t>& buffer) {
+bool sendSCSICommand(int command, std::vector<uint8_t>& commandBuffer,
+                     int dataDirection, std::vector<uint8_t>& dataBuffer) {
     int fd = open(SCSI_DEVICE_PATH, O_RDWR | O_NONBLOCK);
     if (fd == -1) {
         std::cout << "Error opening SCSI device.\n";
@@ -15,29 +16,16 @@ bool readDataSCSI(int sector, int length, std::vector<uint8_t>& buffer) {
     }
 
     sg_io_hdr_t io_hdr{};
-    std::vector<uint8_t> cdb(10);  // SCSI read command
-
-    // Configure the SCSI read command
-    cdb[0] = 0x28;                 // Read command OpCode
-    // Set the LBA (Logical Block Address) and length
-    // in the control bytes of the command
-    cdb[2] = (sector >> 24) & 0xFF;  
-    cdb[3] = (sector >> 16) & 0xFF;
-    cdb[4] = (sector >> 8) & 0xFF;
-    cdb[5] = sector & 0xFF;
-    cdb[7] = (length >> 8) & 0xFF;
-    cdb[8] = length & 0xFF;
-
     io_hdr.interface_id = 'S';
-    io_hdr.cmd_len = cdb.size();
-    io_hdr.cmdp = cdb.data();
-    io_hdr.dxfer_direction = SG_DXFER_FROM_DEV;
-    io_hdr.dxfer_len = length * 512;  // Buffer size in bytes
-    io_hdr.dxferp = buffer.data();
+    io_hdr.cmd_len = commandBuffer.size();
+    io_hdr.cmdp = commandBuffer.data();
+    io_hdr.dxfer_direction = dataDirection;
+    io_hdr.dxfer_len = dataBuffer.size();
+    io_hdr.dxferp = dataBuffer.data();
     io_hdr.timeout = 10000;  // Timeout in milliseconds
 
     if (ioctl(fd, SG_IO, &io_hdr) < 0 || io_hdr.status != 0) {
-        std::cout << "Error during SCSI read operation.\n";
+        std::cout << "Error during SCSI command.\n";
         close(fd);
         return false;
     }
@@ -46,63 +34,146 @@ bool readDataSCSI(int sector, int length, std::vector<uint8_t>& buffer) {
     return true;
 }
 
-bool writeDataSCSI(int sector, int length, const std::vector<uint8_t>& buffer) {
-    int fd = open(SCSI_DEVICE_PATH, O_RDWR | O_NONBLOCK);
-    if (fd == -1) {
-        std::cout << "Error opening SCSI device.\n";
+bool deleteDataSCSI(int sector, int length) {
+    std::vector<uint8_t> commandBuffer(10);  // SCSI command buffer
+    std::vector<uint8_t> dataBuffer(512);    // Data buffer for delete operation
+
+    // Construct the SCSI command for delete operation
+    commandBuffer[0] = 0xXX;  // Set the appropriate SCSI command OpCode
+    // Set the necessary parameters in the commandBuffer
+    // based on the SCSI delete operation
+
+    // Send the SCSI command
+    if (sendSCSICommand(0, commandBuffer, SG_DXFER_NONE, dataBuffer)) {
+        std::cout << "Data deleted successfully.\n";
+        return true;
+    } else {
+        std::cout << "Error deleting data on SCSI device.\n";
         return false;
     }
+}
 
-    sg_io_hdr_t io_hdr{};
-    std::vector<uint8_t> cdb(10);  // SCSI write command
+bool renameDataSCSI(int sector, int length, const std::string& newName) {
+    std::vector<uint8_t> commandBuffer(10);  // SCSI command buffer
+    std::vector<uint8_t> dataBuffer(512);    // Data buffer for rename operation
 
-    // Configure the SCSI write command
-    cdb[0] = 0x2A;                 // Write command OpCode
-    // Set the LBA (Logical Block Address) and length
-    // in the control bytes of the command
-    cdb[2] = (sector >> 24) & 0xFF;
-    cdb[3] = (sector >> 16) & 0xFF;
-    cdb[4] = (sector >> 8) & 0xFF;
-    cdb[5] = sector & 0xFF;
-    cdb[7] = (length >> 8) & 0xFF;
-    cdb[8] = length & 0xFF;
+    // Construct the SCSI command for rename operation
+    commandBuffer[0] = 0xXX;  // Set the appropriate SCSI command OpCode
+    // Set the necessary parameters in the commandBuffer
+    // based on the SCSI rename operation
 
-    io_hdr.interface_id = 'S';
-    io_hdr.cmd_len = cdb.size();
-    io_hdr.cmdp = cdb.data();
-    io_hdr.dxfer_direction = SG_DXFER_TO_DEV;
-    io_hdr.dxfer_len = length * 512;  // Buffer size in bytes
-    io_hdr.dxferp = const_cast<uint8_t*>(buffer.data());
-    io_hdr.timeout = 10000;  // Timeout in milliseconds
-
-    if (ioctl(fd, SG_IO, &io_hdr) < 0 || io_hdr.status != 0) {
-        std::cout << "Error during SCSI write operation.\n";
-        close(fd);
+    // Send the SCSI command
+    if (sendSCSICommand(0, commandBuffer, SG_DXFER_NONE, dataBuffer)) {
+        std::cout << "Data renamed successfully.\n";
+        return true;
+    } else {
+        std::cout << "Error renaming data on SCSI device.\n";
         return false;
     }
+}
 
-    close(fd);
-    return true;
+bool restoreDataSCSI(int sector, int length) {
+    std::vector<uint8_t> commandBuffer(10);  // SCSI command buffer
+    std::vector<uint8_t> dataBuffer(512);    // Data buffer for restore operation
+
+    // Construct the SCSI command for restore operation
+    commandBuffer[0] = 0xXX;  // Set the appropriate SCSI command OpCode
+    // Set the necessary parameters in the commandBuffer
+    // based on the SCSI restore operation
+
+    // Send the SCSI command
+    if (sendSCSICommand(0, commandBuffer, SG_DXFER_NONE, dataBuffer)) {
+        std::cout << "Data restored successfully.\n";
+        return true;
+    } else {
+        std::cout << "Error restoring data on SCSI device.\n";
+        return false;
+    }
+}
+
+bool moveDataSCSI(int sourceSector, int destinationSector, int length) {
+    std::vector<uint8_t> commandBuffer(10);  // SCSI command buffer
+    std::vector<uint8_t> dataBuffer(512);    // Data buffer for move operation
+
+    // Construct the SCSI command for move operation
+    commandBuffer[0] = 0xXX;  // Set the appropriate SCSI command OpCode
+    // Set the necessary parameters in the commandBuffer
+    // based on the SCSI move operation
+
+    // Send the SCSI command
+    if (sendSCSICommand(0, commandBuffer, SG_DXFER_NONE, dataBuffer)) {
+        std::cout << "Data moved successfully.\n";
+        return true;
+    } else {
+        std::cout << "Error moving data on SCSI device.\n";
+        return false;
+    }
 }
 
 int main() {
-    std::vector<uint8_t> buffer(512);  // Buffer for SCSI data
-
-    // Example usage: Read data from SCSI device
-    if (readDataSCSI(0, 1, buffer)) {
-        std::cout << "Data read successfully.\n";
-        // Do something with the read data...
+    // Example usage: Delete data on SCSI device
+    if (deleteDataSCSI(0, 1)) {
+        // Handle successful delete operation
     } else {
-        std::cout << "Error reading data from SCSI device.\n";
+        // Handle error during delete operation
     }
 
-    // Example usage: Write data to SCSI device
+    // Example usage: Rename data on SCSI device
+    if (renameDataSCSI(0, 1, "new_name.txt")) {
+        // Handle successful rename operation
+    } else {
+        // Handle error during rename operation
+    }
+
+    // Example usage: Restore data on SCSI device
+    if (restoreDataSCSI(0, 1)) {
+        // Handle successful restore operation
+    } else {
+        // Handle error during restore operation
+    }
+
+    // Example usage: Move data on SCSI device
+    if (moveDataSCSI(0, 10, 1)) {
+        // Handle successful move operation
+    } else {
+        // Handle error during move operation
+    }
+
+       // Example usage: Write data to SCSI device
     // std::vector<uint8_t> writeBuffer(512);  // Buffer with data to write
     // Initialize writeBuffer with data to write...
     // if (writeDataSCSI(0, 1, writeBuffer)) {
     //     std::cout << "Data written successfully.\n";
     // } else {
     //     std::cout << "Error writing data to SCSI device.\n";
+    // }
+
+    // Example usage: Delete data on SCSI device
+    // if (deleteDataSCSI(0, 1)) {
+    //     std::cout << "Data deleted successfully.\n";
+    // } else {
+    //     std::cout << "Error deleting data on SCSI device.\n";
+    // }
+
+    // Example usage: Rename data on SCSI device
+    // if (renameDataSCSI(0, 1, "new_name.txt")) {
+    //     std::cout << "Data renamed successfully.\n";
+    // } else {
+    //     std::cout << "Error renaming data on SCSI device.\n";
+    // }
+
+    // Example usage: Restore data on SCSI device
+    // if (restoreDataSCSI(0, 1)) {
+    //     std::cout << "Data restored successfully.\n";
+    // } else {
+    //     std::cout << "Error restoring data on SCSI device.\n";
+    // }
+
+    // Example usage: Move data on SCSI device
+    // if (moveDataSCSI(0, 10, 1)) {
+    //     std::cout << "Data moved successfully.\n";
+    // } else {
+    //     std::cout << "Error moving data on SCSI device.\n";
     // }
 
     return 0;
